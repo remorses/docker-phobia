779abc73b4e08380489feb38405a7e2f8c45ecac
remorses <beats.by.morse@gmail.com>
2024-04-28 13:29:15 +0200

    website/next.config: redirect

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -3,6 +3,16 @@ import { withElacca } from 'elacca'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
+    async redirects() {
+        // redirect / to github
+        return [
+            {
+                source: '/',
+                destination: 'https://github.com/remorses/docker-phobia',
+                permanent: false,
+            },
+        ]
+    },
 }
 
 export default withElacca()(nextConfig)
